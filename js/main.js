116888b31b9890efbd80f44aa72d71798c03aba4
onelittlewolfe <onelittlewolfe@gmail.com>
2016-04-21 20:25:29 +0100

    player moving

--- js/main.js
+++ js/main.js
@@ -3,17 +3,21 @@ window.addEventListener("load" , function(){
 	var mainState = {
 	
 		preload : function () {
-			this.hero = game.load.image('hero', 'assets/mario.gif');
-			
+			this.hero = game.load.image('hero', 'assets/player.png');
 		},
 		
 		create : function() {
 			this.hero = game.add.sprite(20, 20, 'hero');
+			game.physics.startSystem(Phaser.Physics.ARCADE);
 			game.physics.arcade.enable(this.hero);
+			
 			this.cursor = game.input.keyboard.createCursorKeys();
 		},
 	
 		update: function() {
+		    this.hero.body.velocity.x = 0;
+		    this.hero.body.velocity.y = 0;
+			
 			this.movePlayer();
 		},
 		
@@ -24,14 +28,17 @@ window.addEventListener("load" , function(){
 		    }  
 			else if (this.cursor.right.isDown) { 
 				this.hero.body.velocity.x = 200;
+			} else if (this.cursor.up.isDown) {
+				this.hero.body.velocity.y = -200;
+				
+			}  else if (this.cursor.down.isDown) {
+				this.hero.body.velocity.y = 200;
+				
 			}
 			else {
 				this.hero.body.velocity.x = 0;
 			}
 		          
-			if (this.cursor.up.isDown && this.hero.body.touching.down) { 
-				 this.hero.body.velocity.y = -320;
-			}
 		}
 	}
 
@@ -39,6 +46,5 @@ window.addEventListener("load" , function(){
 
 	game.state.add( 'main', mainState );
 	game.state.start('main');
-	
 		
 });
