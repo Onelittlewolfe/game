a8e91c1bea3dcaf1a4d33a80aa72c5830b6010b7
onelittlewolfe <onelittlewolfe@gmail.com>
2016-04-21 20:54:06 +0100

    bullets

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 window.addEventListener("load" , function(){
 
 	var mainState = {
+		
+		bulletTime: 0,
 	
 		preload : function () {
 			this.hero = game.load.image('hero', 'assets/player.png');
+			this.bullet = game.load.image('bullet', 'assets/bullet.png');
 		},
 		
 		create : function() {
@@ -12,6 +15,16 @@ window.addEventListener("load" , function(){
 			game.physics.arcade.enable(this.hero);
 			
 			this.cursor = game.input.keyboard.createCursorKeys();
+			fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+			
+		    bullets = game.add.group();
+		    bullets.enableBody = true;
+		    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+		    bullets.createMultiple(30, 'bullet');
+		    bullets.setAll('anchor.x', -0.5);
+		    bullets.setAll('anchor.y', 1);
+		    bullets.setAll('outOfBoundsKill', true);
+		    bullets.setAll('checkWorldBounds', true);
 		},
 	
 		update: function() {
@@ -38,8 +51,40 @@ window.addEventListener("load" , function(){
 			else {
 				this.hero.body.velocity.x = 0;
 			}
+	
+	        if (fireButton.isDown) {
+	            this.fireBullet();
+	        }
+			
 		          
+		},
+		
+		fireBullet: function() {
+
+		    //  To avoid them being allowed to fire too fast we set a time limit
+		    if (game.time.now > this.bulletTime)
+		    {
+		        //  Grab the first bullet we can from the pool
+		        bullet = bullets.getFirstExists(false);
+
+		        if (bullet)
+		        {
+		            //  And fire it
+		            bullet.reset(this.hero.x, this.hero.y + 8);
+		            bullet.body.velocity.y = -400;
+		            bulletTime = game.time.now + 200;
+		        }
+		    }
+
+		},
+		
+		resetBullet: function(bullet) {
+
+		    //  Called if the bullet goes out of the screen
+		    bullet.kill();
+
 		}
+		
 	}
 
 	var game = new Phaser.Game(400, 300, Phaser.AUTO, 'gameDiv');
