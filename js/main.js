eb81674ae26ecb170227ed69988c61c5c1b84507
onelittlewolfe <onelittlewolfe@gmail.com>
2016-05-23 22:00:24 +0100

    more stuff

--- js/main.js
+++ js/main.js
@@ -11,41 +11,67 @@ window.addEventListener("load" , function(){
 			game.load.image('wallH', 'assets/wallHorizontal.png' );
 		},
 		
+		
+		createWorld : function (){
+			
+			this.walls = game.add.group();
+			this.walls.enableBody = true;
+			
+					
+			var leftWall = game.add.sprite(0, 0, 'wallV', 0, this.walls);
+			leftWall.scale.setTo(1.5, 5);
+			
+			var rightWall = game.add.sprite(770, 0, 'wallV', 0, this.walls);
+			rightWall.scale.setTo(1.5, 5);
+			
+			var floor = game.add.sprite(0, 460, 'wallH', 0, this.walls);
+			floor.scale.setTo(3, 1);
+			
+			var celing = game.add.sprite(0, 0, 'wallH', 0, this.walls);
+			celing.scale.setTo(4, 1);
+			
+			this.walls.setAll('body.immovable', true);
+			
+		},
+		
+		createBullets: function (){
+		    
+			this.bullets = game.add.group();
+		    this.bullets.enableBody = true;
+		    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+		    this.bullets.createMultiple(40, 'bullet');
+		    this.bullets.setAll('anchor.x', -0.5);
+		    this.bullets.setAll('anchor.y', 1);
+		    this.bullets.setAll('outOfBoundsKill', true);
+		    this.bullets.setAll('checkWorldBounds', true);
+		},
+		
 		create : function() {
 			this.hero = game.add.sprite(game.world.centerX, game.world.centerY, 'hero');
-			this.wallLeft = game.add.sprite(0, 0, 'wallV');
 			
 			game.physics.startSystem(Phaser.Physics.ARCADE);
 			
 			game.physics.arcade.enable(this.hero);
-			game.physics.arcade.enable(this.wallLeft);
-			
-			this.wallLeft.body.immovable = true;
-			
-			this.wallLeft.scale.setTo(1.5, 5);
 			
 			this.cursor = game.input.keyboard.createCursorKeys();
 			fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 			
-		    bullets = game.add.group();
-		    bullets.enableBody = true;
-		    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-		    bullets.createMultiple(40, 'bullet');
-		    bullets.setAll('anchor.x', -0.5);
-		    bullets.setAll('anchor.y', 1);
-		    bullets.setAll('outOfBoundsKill', true);
-		    bullets.setAll('checkWorldBounds', true);
+			this.createBullets();
+			this.createWorld();
 		},
 	
 		update: function() {
-			
 
-			game.physics.arcade.collide(this.hero, this.wallLeft);
+			game.physics.arcade.collide(this.hero, this.walls);
 			
 		    this.hero.body.velocity.x = 0;
 		    this.hero.body.velocity.y = 0;
 			
 			this.movePlayer();
+			
+			if ( !this.hero.inWorld ) { 
+				this.playerDie();
+			}
 		},
 		
 		movePlayer: function() {
@@ -73,13 +99,19 @@ window.addEventListener("load" , function(){
 		          
 		},
 		
+		playerDie: function(){
+			
+			game.state.start('main');
+			
+		},
+		
 		fireBullet: function() {
 
 		    //  To avoid them being allowed to fire too fast we set a time limit
 		    if (game.time.now > this.bulletTime)
 		    {
 		        //  Grab the first bullet we can from the pool
-		        bullet = bullets.getFirstExists(false);
+		        bullet = this.bullets.getFirstExists(false);
 
 		        if (bullet)
 		        {
